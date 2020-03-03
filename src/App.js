11c518fcc987ea821a70387667d5d29b68a63314
Arthur Lima <arthur-david@hotmail.com>
2020-03-03 13:34:57 -0300

    implementing sign in

--- src/App.js
+++ src/App.js
@@ -73,6 +73,10 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
+  onRouteChange = () => {
+    this.setState({route: 'home'});
+  }
+
 
   render() {
     return (
@@ -81,11 +85,15 @@ class App extends Component {
           params={particlesOptions}
         />
         <Navigation />
-        <Signin/>
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        { this.state.route === 'signin' 
+          ? <Signin onRouteChange={this.onRouteChange}/>
+          : <div>
+                <Logo />
+                <Rank />           
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+           </div> 
+        }
       </div>
     );
   }
